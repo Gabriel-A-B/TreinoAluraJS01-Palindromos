ad905a97231e4af2bf1f1ac0e27f6eff4129731f
GabrielAndrade <91852179+Gabriel-A-B@users.noreply.github.com>
2024-05-11 11:52:42 -0300

    palindromo
    Apenas e um exercício de reforço simples

--- treino.js
+++ treino.js
@@ -1,6 +1,19 @@
-const corpo = document.getElementById('palavra')
-const palavra = ''
+const resultado = document.getElementById('resultado')
+const caixaDeTexto = document.getElementById('palavra')
+const enviar = document.getElementById('enviar')
 
-function verificaçãoPalindromica() {
-    
-}
+enviar.addEventListener('click', () => {
+    debugger
+    let resposta = caixaDeTexto.value
+    let respostaNormal = caixaDeTexto.value
+    resposta.split('').reverse().join('')
+    if (resposta == respostaNormal) {
+        resultado.innerHTML = `
+        <h1 class="resultado_titulo">Essa palavra é um Palindromo</h1>
+        `
+    }else {
+        resultado.innerHTML = `
+        <h1 class="resultado_titulo">Essa palavra não é um Palindromo</h1>
+        `
+    }
+})
